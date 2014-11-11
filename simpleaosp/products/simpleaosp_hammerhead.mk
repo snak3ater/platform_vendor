c5c44364312eeb3a4ef59ed2d27842a0342d59d8
MrBaNkS <banksmi09@gmail.com>
2014-11-10 18:16:31 -0600

    Move green bar fix to vendor/lge

--- simpleaosp/products/simpleaosp_hammerhead.mk
+++ simpleaosp/products/simpleaosp_hammerhead.mk
@@ -13,7 +13,3 @@ PRODUCT_MANUFACTURER := LGE
 
 # Build prop fingerprint overrides
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="hammerhead" BUILD_FINGERPRINT="google/hammerhead/hammerhead:5.0/LRX21M/1551133:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 5.0 LRX21M 1551133 release-keys"
-
-# Proprietary libmmcamera_interface.so because of green bar bug
-PRODUCT_COPY_FILES += \
-    vendor/simpleaosp/prebuilts/common/system/lib/libmmcamera_interface.so:system/lib/libmmcamera_interface.so
