915d712a5a237112e411eda9ab811c7db052753d
MrBaNkS <banksmi09@gmail.com>
2014-11-14 23:56:16 -0600

    Update all build fingerprints

--- simpleaosp/products/simpleaosp_flo.mk
+++ simpleaosp/products/simpleaosp_flo.mk
@@ -12,4 +12,4 @@ PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := ASUS
 
 # Build prop fingerprint overrides
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="razor" BUILD_FINGERPRINT="google/razor/flo:5.0/LRX21Q/1570415:user/release-keys" PRIVATE_BUILD_DESC="razor-user 5.0 LRX21Q 1570415 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="razor" BUILD_FINGERPRINT="google/razor/flo:5.0/LRX21T/1576899:user/release-keys" PRIVATE_BUILD_DESC="razor-user 5.0 LRX21T 1576899 release-keys"
--- simpleaosp/products/simpleaosp_hammerhead.mk
+++ simpleaosp/products/simpleaosp_hammerhead.mk
@@ -12,4 +12,4 @@ PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
 
 # Build prop fingerprint overrides
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="hammerhead" BUILD_FINGERPRINT="google/hammerhead/hammerhead:5.0/LRX21Q/1570415:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 5.0 LRX21Q 1570415 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="hammerhead" BUILD_FINGERPRINT="google/hammerhead/hammerhead:5.0/LRX21T/1576899:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 5.0 LRX21T 1576899 release-keys"
