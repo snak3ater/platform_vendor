8559546a45c8cfe279752485c43b25c61663942a
MrBaNkS <banksmi09@gmail.com>
2014-11-19 06:03:53 -0600

    Update build fingerprints

--- simpleaosp/products/simpleaosp_mako.mk
+++ simpleaosp/products/simpleaosp_mako.mk
@@ -12,4 +12,4 @@ PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
 
 # Build prop fingerprint overrides
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="occam" BUILD_FINGERPRINT="google/occam/mako:5.0/LRX21T/1576899:user/release-keys" PRIVATE_BUILD_DESC="occam-user 5.0 LRX21T 1576899 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="occam" BUILD_FINGERPRINT="google/occam/mako:5.0/LRX21V/1576899:user/release-keys" PRIVATE_BUILD_DESC="occam-user 5.0 LRX21V 1576899 release-keys"
