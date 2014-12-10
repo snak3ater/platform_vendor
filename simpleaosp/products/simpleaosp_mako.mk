96c487886d477f7bd95b627e12fecdbbeba69867
MrBaNkS <banksmi09@gmail.com>
2014-12-10 05:30:14 -0600

    Go back to using genuine build fingerprints

--- simpleaosp/products/simpleaosp_mako.mk
+++ simpleaosp/products/simpleaosp_mako.mk
@@ -11,5 +11,8 @@ PRODUCT_DEVICE := mako
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
 
-# Build prop fingerprint overrides
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="occam" BUILD_FINGERPRINT="google/occam/mako:5.0.1/LRX22C/1602158:user/release-keys" PRIVATE_BUILD_DESC="occam-user 5.0.1 LRX22C 1602158 release-keys"
+# Build fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="occam" \
+    BUILD_FINGERPRINT="google/occam/mako:5.0/LRX21T/1576899:user/release-keys" \
+    PRIVATE_BUILD_DESC="occam-user 5.0 LRX21T 1576899 release-keys"
