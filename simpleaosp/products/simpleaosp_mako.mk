4eaabd7522f285b5f38300143e9720475df531bc
MrBaNkS <banksmi09@gmail.com>
2014-12-11 21:01:45 -0600

    Update build fingerprints for mako and shamu

--- simpleaosp/products/simpleaosp_mako.mk
+++ simpleaosp/products/simpleaosp_mako.mk
@@ -14,5 +14,5 @@ PRODUCT_MANUFACTURER := LGE
 # Build fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="occam" \
-    BUILD_FINGERPRINT="google/occam/mako:5.0/LRX21T/1576899:user/release-keys" \
-    PRIVATE_BUILD_DESC="occam-user 5.0 LRX21T 1576899 release-keys"
+    BUILD_FINGERPRINT="google/occam/mako:5.0.1/LRX22C/1602158:user/release-keys" \
+    PRIVATE_BUILD_DESC="occam-user 5.0.1 LRX22C 1602158 release-keys"
