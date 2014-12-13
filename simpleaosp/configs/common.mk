74970cdf9bb330b72b7a307e69f8d8fd7f0169e8
MrBaNkS <banksmi09@gmail.com>
2014-12-13 13:59:37 -0600

    Organize and cleanup sa vendor
    1. Add custom bootanimation by PartimusPrime (thanks) :)
    2. Move bootanimations to a separate mk file (No shamu bootanimation yet so fallback to stock L bootanimation for now)
    3. Move hammerhead specific bits to hammerhead product mk file
    4. General organizing

--- simpleaosp/configs/common.mk
+++ simpleaosp/configs/common.mk
@@ -32,13 +32,10 @@ PRODUCT_PACKAGES += \
 
 # Stk for only telephony devices
 ifneq ($(filter simpleaosp_hammerhead simpleaosp_mako simpleaosp_shamu,$(TARGET_PRODUCT)),)
-PRODUCT_PACKAGES += Stk
+ PRODUCT_PACKAGES += Stk
 endif
 
-# Bootanimation
-PRODUCT_BOOTANIMATION := $(LOCAL_PATH)/media/bootanimation.zip
-
-# Proprietary latinime lib needed for swyping
+# Proprietary keyboard lib needed for swyping
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/lib/libjni_latinime.so:system/lib/libjni_latinime.so
 
@@ -57,9 +54,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bin/backuptool.functions:system/bin/backuptool.functions \
     $(LOCAL_PATH)/bin/backuptool.sh:system/bin/backuptool.sh
 
-# Hammerhead greenbar camera recording fix & time update fix
-ifneq ($(filter simpleaosp_hammerhead,$(TARGET_PRODUCT)),)
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/app/TimeService/TimeService.apk:system/app/TimeService/TimeService.apk \
-    $(LOCAL_PATH)/lib/libmmcamera_interface.so:system/lib/libmmcamera_interface.so
-endif
+# Include bootanimation mk file
+-include vendor/simpleaosp/configs/bootanimation.mk
--- simpleaosp/products/simpleaosp_hammerhead.mk
+++ simpleaosp/products/simpleaosp_hammerhead.mk
@@ -1,3 +1,6 @@
+# Local path for hammerhead prebuilts
+LOCAL_PATH := vendor/simpleaosp/prebuilts/hammerhead/system
+
 # Inherit AOSP device configuration for hammerhead
 $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
 
@@ -16,3 +19,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="hammerhead" \
     BUILD_FINGERPRINT="google/hammerhead/hammerhead:5.0/LRX21O/1570415:user/release-keys" \
     PRIVATE_BUILD_DESC="hammerhead-user 5.0 LRX21O 1570415 release-keys"
+
+# Hammerhead greenbar camera recording fix & time update fix
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/app/TimeService/TimeService.apk:system/app/TimeService/TimeService.apk \
+    $(LOCAL_PATH)/lib/libmmcamera_interface.so:system/lib/libmmcamera_interface.so
