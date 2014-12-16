ebf55da4b91120f46c0dc69b772ef41eb4949254
MrBaNkS <banksmi09@gmail.com>
2014-12-16 01:35:07 -0600

    Update build fingerprint for hammerhead

--- simpleaosp/products/simpleaosp_hammerhead.mk
+++ simpleaosp/products/simpleaosp_hammerhead.mk
@@ -17,8 +17,8 @@ PRODUCT_MANUFACTURER := LGE
 # Build fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="hammerhead" \
-    BUILD_FINGERPRINT="google/hammerhead/hammerhead:5.0/LRX21O/1570415:user/release-keys" \
-    PRIVATE_BUILD_DESC="hammerhead-user 5.0 LRX21O 1570415 release-keys"
+    BUILD_FINGERPRINT="google/hammerhead/hammerhead:5.0.1/LRX22C/1602158:user/release-keys" \
+    PRIVATE_BUILD_DESC="hammerhead-user 5.0.1 LRX22C 1602158 release-keys"
 
 # Hammerhead greenbar camera recording fix & time update fix
 PRODUCT_COPY_FILES += \
