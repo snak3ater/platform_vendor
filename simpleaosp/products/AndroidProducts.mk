1eafb082e711a106d57d71dae65ed7e1c953a2e2
snak3ater <saeedyasir91@gmail.com>
2014-12-26 17:30:29 +0500

    Remove Shamu and Flo

--- simpleaosp/products/AndroidProducts.mk
+++ simpleaosp/products/AndroidProducts.mk
@@ -1,5 +1,3 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/simpleaosp_flo.mk \
     $(LOCAL_DIR)/simpleaosp_hammerhead.mk \
     $(LOCAL_DIR)/simpleaosp_mako.mk \
-    $(LOCAL_DIR)/simpleaosp_shamu.mk
--- simpleaosp/vendorsetup.sh
+++ simpleaosp/vendorsetup.sh
@@ -1,4 +1,3 @@
 add_lunch_combo simpleaosp_flo-user
 add_lunch_combo simpleaosp_hammerhead-user
 add_lunch_combo simpleaosp_mako-user
-add_lunch_combo simpleaosp_shamu-user
