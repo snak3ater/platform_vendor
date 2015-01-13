2c1876f0479b5e895cb1871b0d5b7c24b8a7fe7b
MrBaNkS <banksmi09@gmail.com>
2015-01-13 11:57:52 -0600

    Update sm.mk

--- configs/sm.mk
+++ configs/sm.mk
@@ -17,8 +17,8 @@ ifeq (linux,$(HOST_OS))
 # Filter by TARGET_ARCH since we're pointing to ARCH specific compilers.
 # To use this on new devices define TARGET_ARCH in device makefile.
 ifeq (arm,$(TARGET_ARCH))
-export LD_LIBRARY_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8/arch-arm/usr/lib
-export LIBRARY_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8/arch-arm/usr/lib
+export LD_LIBRARY_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8/arch-arm/usr/lib
+export LIBRARY_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8/arch-arm/usr/lib
 
 # Path to toolchain
 SM_AND_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8
@@ -53,8 +53,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 endif
 
 ifeq (arm64,$(TARGET_ARCH))
-export LD_LIBRARY_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9/arch-arm64/usr/lib
-export LIBRARY_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9/arch-arm64/usr/lib
+export LD_LIBRARY_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9/arch-arm64/usr/lib
+export LIBRARY_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9/arch-arm64/usr/lib
 
 # Path to toolchain
 SM_AND_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9
